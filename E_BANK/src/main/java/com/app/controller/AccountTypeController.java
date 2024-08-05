1a6bf120e49bfdb9f3c39149205e2f86b806be4d
vigh102 <vighnesh.masurkar@hotmail.com>
2024-08-06 01:01:16 +0530

    update AccountTypeController updated

--- E_BANK/src/main/java/com/app/controller/AccountTypeController.java
+++ E_BANK/src/main/java/com/app/controller/AccountTypeController.java
@@ -2,6 +2,7 @@ package com.app.controller;
 
 import java.util.List;
 
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +27,12 @@ import com.app.service.AccountTypeService;
 @Validated
 public class AccountTypeController {
 	
-	@Autowired(required=true)
+//	@Autowired
 	private AccountTypeService accService;
+//	
+//	@Autowired(required=true)
+//	private AccountTypeService accService;
+//	
 	
 	@GetMapping
 	public ResponseEntity<?> getAllAccountType()
