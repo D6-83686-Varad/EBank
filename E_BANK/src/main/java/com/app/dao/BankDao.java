febb9369ea19eda0551967d29c283862ea05719e
Varadkkulkarni <varad.kulkarni2201@gmail.com>
2024-08-05 22:34:01 +0530

    DAO Added
    Adding All DAOs

--- E_BANK/src/main/java/com/app/dao/BankDao.java
+++ E_BANK/src/main/java/com/app/dao/BankDao.java
@@ -3,9 +3,9 @@ package com.app.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.app.entity.account.Account;
+import com.app.entity.bank.Bank;
 
 @Repository
-public interface AccountDao extends JpaRepository<Account, String > {
-
+public interface BankDao extends JpaRepository<Bank,String > {
+	
 }
--- E_BANK/src/main/java/com/app/dao/TransactionHistoryDao.java
+++ E_BANK/src/main/java/com/app/dao/TransactionHistoryDao.java
@@ -3,9 +3,10 @@ package com.app.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.app.entity.account.Account;
+import com.app.entity.payment.TransactionHistory;
+
 
 @Repository
-public interface AccountDao extends JpaRepository<Account, String > {
+public interface TransactionHistoryDao extends JpaRepository<TransactionHistory, String> {
 
 }
