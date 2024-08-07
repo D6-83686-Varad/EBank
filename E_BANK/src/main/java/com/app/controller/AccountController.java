ab33b1788f42bf3d013a1ca77f3993ecda7fd4ed
vigh102 <vighnesh.masurkar@hotmail.com>
2024-08-07 09:01:06 +0530

    account Controller methods added

--- E_BANK/src/main/java/com/app/controller/AccountController.java
+++ E_BANK/src/main/java/com/app/controller/AccountController.java
@@ -1,5 +1,68 @@
 package com.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.entity.account.Account;
+import com.app.service.AccountSevice;
+
+@RestController
+@RequestMapping("/account")
 public class AccountController {
+	
+	@Autowired
+	private AccountSevice accService;
+	
+
+	@GetMapping("/actiavted")
+	public ResponseEntity<?> getAllActivatedAccounts()
+	{
+		List<Account> activatedAccounts =accService.getAllActivatedAccount();
+		if(activatedAccounts.isEmpty())
+		{
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(activatedAccounts);
+	}
+	
+	@GetMapping("/deactivated")
+	public ResponseEntity<?> getAllDeactivatedAccounts()
+	{
+		List<Account> deactivatedAccounts =accService.getAllDeactivatedAccount();
+		if(deactivatedAccounts.isEmpty())
+		{
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(deactivatedAccounts);
+	}
+	
+	@GetMapping("/suspended")
+	public ResponseEntity<?> getAllSuspendedAccounts()
+	{
+		List<Account> suspendedAccounts =accService.getAllSuspendedAccount();
+		if(suspendedAccounts.isEmpty())
+		{
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(suspendedAccounts);
+	}
+	@PatchMapping("/{accId}/updateStatusA")
+	public ResponseEntity<?> changeToActivate(@PathVariable("accId")String accId)
+	{
+		return ResponseEntity.ok(accService.changeStatusOfActivatedAccount(accId));
+	}
+	@PatchMapping("/{accId}/updateStatusD")
+	public ResponseEntity<?> changeToDeactivate(@PathVariable("accId")String accId)
+	{
+		return ResponseEntity.ok(accService.changeStatusOfDeactivatedAccount(accId));
+	}
+	
 
 }
