41a43ccfcd1fff32022c9a2f4829d8d432e55ada
Varadkkulkarni <varad.kulkarni2201@gmail.com>
2024-08-17 12:37:36 +0530

    LOAN deleted

--- Loan/src/main/java/com/app/loan/dao/LoanDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.loan.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.loan.entities.Loan;
-
-public interface LoanDao extends JpaRepository<Loan, String>{
-
-}
